ba58afd7213be496064f4e66aac9a230a4e7344e
Kuldeep Kumar <kuldeephrk3@outlook.com>
2021-02-08 08:30:10 +0530

    Added Projects

---
+++ AbstractFactory/Program.cs
@@ -0,0 +1,32 @@
+﻿using Interfaces;
+using Providers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractFactory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> accntNumbers = new List<string> {"CITI-456", "NATIONAL-987", "CHASE-222"};
+            foreach (var account in accntNumbers)
+            {
+                ICreditUnionFactory anAbstractFactory = CreditUnionFactoryProvider.GetCreditUnionFactory(account);
+                if (anAbstractFactory == null)
+                {
+                    Console.WriteLine("Sorry. This credit union w/ account number" + " ' {0} ' is invalid.", (account));
+                }
+                else
+                {
+                    ILoanAccount loan = anAbstractFactory.CreateLoanAccount();
+                    ISavingsAccount savings = anAbstractFactory.CreateSavingsAccount();
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Singleton/Policy.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    public class Policy
+    {
+        private int Id { get; set; } = 123;
+        private string Insured { get; set; } = "John Roy";
+        // Method 1
+        private static Policy _instance;
+        public static Policy Instance => _instance ?? (_instance = new Policy());
+        // Method 2
+        private static readonly Policy _instance2 = new Policy();
+        public static Policy Instance2 => _instance2;
+
+        public Policy() { }
+        public string GetInsuredName() => Insured;
+    }
+}
